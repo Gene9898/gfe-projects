1b85b273a9c958d73db6d94f9c6714388e1497c3
Eugene Lum <eugenelumtf@gmail.com>
2024-11-19 01:29:03 +0800

    change to nextjs

---
+++ project-challenges/pages/challenge1/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Challenge1 = () => {
+  return (
+    <div class="mt-[200px] bg-white flex flex-col rounded-lg w-[340px] h-[233px] p-[24px] gap-4 shadow-md mx-auto">
+      <div class="flex flex-row gap-4">
+        <img src="profile-thumbnail.png" alt="Profile Picture" width="48" height="48"/>
+        <div>
+          <h1 class="text-lg font-semibold">Sarah Dole</h1>
+          <p class="text-neutral-600 text-sm">@sarahdole</p>
+        </div>
+      </div>
+      <p class="text-neutral-600 text-base">
+        I've been searching for high-quality abstract images for my design
+        projects, and I'm thrilled to have found this platform. The variety and
+        depth of creativity are astounding!
+      </p>
+    </div>
+  )
+}
+
+export default Challenge1
---
+++ project-challenges/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+	return <h1 className="text-pink-500">Project Challenges</h1>;
+};
+
+export default Home;
