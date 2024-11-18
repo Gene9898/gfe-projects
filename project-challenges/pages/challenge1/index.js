63273d31ed2e0b0a3f560692c7b35794d46fdd67
Eugene Lum <eugenelumtf@gmail.com>
2024-11-19 01:42:11 +0800

    fixed bg colour

--- project-challenges/pages/challenge1/index.js
+++ project-challenges/pages/challenge1/index.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Challenge1 = () => {
-  return (
-    <div class="mt-[200px] bg-white flex flex-col rounded-lg w-[340px] h-[233px] p-[24px] gap-4 shadow-md mx-auto">
-      <div class="flex flex-row gap-4">
-        <img src="profile-thumbnail.png" alt="Profile Picture" width="48" height="48"/>
-        <div>
-          <h1 class="text-lg font-semibold">Sarah Dole</h1>
-          <p class="text-neutral-600 text-sm">@sarahdole</p>
-        </div>
-      </div>
-      <p class="text-neutral-600 text-base">
-        I've been searching for high-quality abstract images for my design
-        projects, and I'm thrilled to have found this platform. The variety and
-        depth of creativity are astounding!
-      </p>
-    </div>
-  )
-}
+	return (
+		<div className="flex justify-center bg-gradient-to-br from-[#F9FAFB] to-[#D2D6DB] h-screen w-screen">
+			<div class="mt-[200px] bg-white flex flex-col rounded-lg w-[340px] h-[233px] p-[24px] gap-4 shadow-md">
+				<div class="flex flex-row gap-4">
+					<img
+						src="profile-thumbnail.png"
+						alt="Profile Picture"
+						width="48"
+						height="48"
+					/>
+					<div>
+						<h1 class="text-lg font-semibold">Sarah Dole</h1>
+						<p class="text-neutral-600 text-sm">@sarahdole</p>
+					</div>
+				</div>
+				<p class="text-neutral-600 text-base">
+					I've been searching for high-quality abstract images for my design
+					projects, and I'm thrilled to have found this platform. The variety
+					and depth of creativity are astounding!
+				</p>
+			</div>
+		</div>
+	);
+};
 
-export default Challenge1
+export default Challenge1;
